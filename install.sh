baf03a1c740a46e66e07eaff33336ca509ccb93e
MikeAScott <mr.mike.a.scott@gmail.com>
2019-10-25 19:15:04 +0100

    Allow SELinux read write on logs

--- install.sh
+++ install.sh
@@ -4,17 +4,17 @@
 
 SITE="helloworld"
 TLD="local"
-APP="helloworldapp"
 SITE_PATH="/var/www/$SITE"
 CONF_FILE="$SITE.conf"
 SOURCE=$(pwd)
-
 
 mkdir -p /etc/httpd/sites-available 
 mkdir -p /etc/httpd/sites-enabled
 mkdir -p $SITE_PATH
 mkdir -p $SITE_PATH/wsgi
 mkdir -p $SITE_PATH/logs
+semanage fcontext -a -t httpd_sys_rw_content_t $SITE_PATH/logs
+restorecon -v $SITE_PATH/logs
 
 # use a2ensite ? a2ensite $CONF_FILE
 cp conf/$CONF_FILE /etc/httpd/sites-available/
